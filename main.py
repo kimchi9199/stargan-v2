92de95beebef6827786418549fd5750b24f28683
Kim Lê <91075598+kimchi9199@users.noreply.github.com>
2022-08-29 22:30:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -104,20 +104,20 @@ if __name__ == '__main__':
                         help='Weight for style reconstruction loss')
     parser.add_argument('--lambda_ds', type=float, default=1,
                         help='Weight for diversity sensitive loss')
-#     parser.add_argument('--ds_iter', type=int, default=100000,
-#                         help='Number of iterations to optimize diversity sensitive loss') giam iteration
-    parser.add_argument('--ds_iter', type=int, default=1000,
-                        help='Number of iterations to optimize diversity sensitive loss')
+    parser.add_argument('--ds_iter', type=int, default=100000,
+                        help='Number of iterations to optimize diversity sensitive loss') giam iteration
+#     parser.add_argument('--ds_iter', type=int, default=1000,
+#                         help='Number of iterations to optimize diversity sensitive loss')
     parser.add_argument('--w_hpf', type=float, default=1,
                         help='weight for high-pass filtering')
 
     # training arguments
     parser.add_argument('--randcrop_prob', type=float, default=0.5,
                         help='Probabilty of using random-resized cropping')
-#     parser.add_argument('--total_iters', type=int, default=100000,
-#                         help='Number of total iterations')  giam iteration
-    parser.add_argument('--total_iters', type=int, default=1000,
-                        help='Number of total iterations')
+    parser.add_argument('--total_iters', type=int, default=100000,
+                        help='Number of total iterations')  giam iteration
+#     parser.add_argument('--total_iters', type=int, default=1000,
+#                         help='Number of total iterations')
     parser.add_argument('--resume_iter', type=int, default=0,
                         help='Iterations to resume training/testing')
     parser.add_argument('--batch_size', type=int, default=8,
@@ -178,12 +178,12 @@ if __name__ == '__main__':
 
     # step size
     parser.add_argument('--print_every', type=int, default=10)
-#     parser.add_argument('--sample_every', type=int, default=5000)
-    parser.add_argument('--sample_every', type=int, default=50)
-#     parser.add_argument('--save_every', type=int, default=10000)
-    parser.add_argument('--save_every', type=int, default=100)
-#     parser.add_argument('--eval_every', type=int, default=50000)
-    parser.add_argument('--eval_every', type=int, default=500)
+    parser.add_argument('--sample_every', type=int, default=5000)
+#     parser.add_argument('--sample_every', type=int, default=50)
+    parser.add_argument('--save_every', type=int, default=10000)
+#     parser.add_argument('--save_every', type=int, default=100)
+    parser.add_argument('--eval_every', type=int, default=50000)
+#     parser.add_argument('--eval_every', type=int, default=500)
 
     args = parser.parse_args()
     main(args)
